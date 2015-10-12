31885931d9487f4cca936ff5ad9d7e05da14d355
Brendan McKenzie <brendan.mckenzie@deepend.com.au>
2015-10-12 17:16:17 +1100

    Fixing project framework references

---
+++ test/Kasbah.Core.Tests/Noop.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Kasbah.Core.Tests
+{
+    public class Noop
+    {
+        #region Public Methods
+
+        [Fact]
+        public void Nothing()
+        {
+            Assert.True(true);
+        }
+
+        #endregion
+    }
+}
